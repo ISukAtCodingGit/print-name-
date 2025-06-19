80e1be28ba7a3ab06a3a29eb817e31deb8f245ff
ISukAtCodingGit <luqi.xu69@gmail.com>
2025-06-19 21:26:41 +1000

    Added player movement

---
+++ scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -200.0
+
+#Get nodes
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	#Handle movement
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		
+	#Handle Animations
+	if direction == 0:
+		animated_sprite.play("Idle")
+	else:
+		animated_sprite.play("move")
+		
+	#Handle Flipping sprites
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+	move_and_slide()
