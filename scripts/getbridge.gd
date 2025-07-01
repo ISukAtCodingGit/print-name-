4b5191cae65f6122bc4192add5e59449a2477f87
ISukAtCodingGit <luqi.xu69@gmail.com>
2025-07-01 19:38:01 +1000

    Fixed dependencies

--- scripts/getbridge.gd
+++ scripts/getbridge.gd
@@ -1,8 +1,15 @@
 extends Area2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var on := false
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		var bridge = get_tree().current_scene.get_node("bridge")
+		if on == false:
+			animated_sprite_2d.play("default")
+			on = true
 		if bridge:
 			bridge.show()
 			if bridge.has_node("CollisionShape2D"):
